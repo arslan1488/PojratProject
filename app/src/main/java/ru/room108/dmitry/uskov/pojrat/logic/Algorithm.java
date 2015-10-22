866d27fdb976bf3ae128ce93976e0a29d19172d4
Usk Dminty <usk-dima2@yandex.ru>
2015-10-22 14:22:48 +0400

    Created template for algorithm

---
+++ app/src/main/java/ru/room108/dmitry/uskov/pojrat/logic/Algorithm.java
@@ -0,0 +1,14 @@
+package ru.room108.dmitry.uskov.pojrat.logic;
+
+
+import java.util.ArrayList;
+
+import ru.room108.dmitry.uskov.pojrat.Item;
+
+public class Algorithm {
+
+    public static ArrayList<Item> getItemByUserPreferences(ArrayList<Item> items, UserPreferences userPreferences){
+        //TODO
+        return null;
+    }
+}
---
+++ app/src/main/java/ru/room108/dmitry/uskov/pojrat/logic/UserPreferences.java
@@ -0,0 +1,22 @@
+package ru.room108.dmitry.uskov.pojrat.logic;
+
+/**
+ * Этот класс вспомогательный для функции расчёта персонального меню по требованиям клиента
+ * в нём должны содержаться все предпочтения клиента
+ * кол-во денег
+ * кол-во персон
+ * Special mod-ы
+ * и тому подобное
+ * объект этого класса будет передоваться в Algorithm.getItemByUserPreferences вместе со списком всех товаров и на основании этих данных
+ * функция Algorithm.getItemByUserPreferences должна выдать список подходящих наборов
+ */
+
+public class UserPreferences {
+
+    double money;
+
+    double countPeople;
+
+    //TODO add fields
+
+}
